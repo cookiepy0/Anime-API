c00d048317d3ca000b433f95e38dc0a3c068e5f6
cookiepy0 <cookieytbrofc2@gmail.com>
2025-02-16 16:20:11 -0300

    Update rateLimit.ts

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 60 minutes
+  windowMs: 1, // 60 minutes
   max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy`` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
